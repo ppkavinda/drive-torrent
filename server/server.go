a0ac986cc8765282eb714172544bae10f39f6248
prasad kavinda <pp.kavinda@gmail.com>
2019-01-26 20:56:55 +0530

    add mux router

---
+++ server/server.go
@@ -0,0 +1,81 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+// Server stores config of server
+type Server struct {
+	Port string
+}
+
+// StartServer will start the http server
+func (s *Server) StartServer() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	registerHandlers()
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func registerHandlers() {
+	r := mux.NewRouter()
+
+	r.Handle("/", http.RedirectHandler("/books", http.StatusFound))
+
+	// The following handlers are defined in auth.go and used in the
+	// "Authenticating Users" part of the Getting Started guide.
+	r.Methods("GET").Path("/login").
+		Handler(appHandler(loginHandler))
+
+	// Respond to App Engine and Compute Engine health checks.
+	// Indicate the server is healthy.
+	r.Methods("GET").Path("/_ah/health").HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("ok"))
+		})
+
+	// [START request_logging]
+	// Delegate all of the HTTP routing and serving to the gorilla/mux router.
+	// Log all requests using the standard Apache format.
+	http.Handle("/", handlers.CombinedLoggingHandler(os.Stderr, r))
+	// [END request_logging]
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) *appError {
+	// w.Write([]byte("asdfsd"))
+	w.WriteHeader(http.StatusOK)
+	return nil
+}
+
+// http://blog.golang.org/error-handling-and-go
+type appHandler func(http.ResponseWriter, *http.Request) *appError
+
+type appError struct {
+	Error   error
+	Message string
+	Code    int
+}
+
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if e := fn(w, r); e != nil { // e is *appError, not os.Error.
+		log.Printf("Handler error: status code: %d, message: %s, underlying err: %#v",
+			e.Code, e.Message, e.Error)
+
+		http.Error(w, e.Message, e.Code)
+	}
+}
+
+func appErrorf(err error, format string, v ...interface{}) *appError {
+	return &appError{
+		Error:   err,
+		Message: fmt.Sprintf(format, v...),
+		Code:    500,
+	}
+}
