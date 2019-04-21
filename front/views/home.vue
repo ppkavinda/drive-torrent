4522070d1a4b2500bbef7f650cb58c15aa9f029f
danushka96 <danushkaherath96@gmail.com>
2019-04-21 20:27:04 +0530

    fix conflict

--- front/views/home.vue
+++ front/views/home.vue
@@ -2,21 +2,15 @@
   <div class="container">
     <div class="row">
         <input-torrent></input-torrent>
-<<<<<<< HEAD
         <torrents></torrents>
       </div>
       <search></search>
       <!-- <film-List></film-List> -->
-=======
-        <search-box></search-box>
-      <film-List></film-List>
->>>>>>> d8a6dbba662b565c9d45529fa7edd5767fdc10b8
     </div>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import InputTorrent from '../components/inputTorrent'
 import Torrents from '../components/torrents'
 // import filmList from '../components/filmList'
@@ -24,15 +18,6 @@ import search from '../components/search'
 
 export default {
   components: {InputTorrent, Torrents, search},
-=======
-import InputTorrent from "../components/inputTorrent";
-import Torrents from "../components/torrents";
-import filmList from "../components/filmList";
-import SearchBox from '../components/searchBox';
-
-export default {
-  components: { InputTorrent, Torrents, filmList, SearchBox },
->>>>>>> d8a6dbba662b565c9d45529fa7edd5767fdc10b8
   data() {
     return {
       user: window.User
