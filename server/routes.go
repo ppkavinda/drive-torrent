7111155f8d8774363d8e5ef54b17b765badc2494
Prasad Kavinda <pp.kavinda@gmail.com>
2019-05-10 23:17:50 +0530

    Revert "release"

--- server/routes.go
+++ server/routes.go
@@ -7,14 +7,10 @@ import (
 	"os"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/gobuffalo/packr"
 )
 
 // here lies all the routes of the app
 func getRoutes(s *Server, r *mux.Router) *mux.Router {
-
-	box := packr.NewBox("../static")
-	index, e := box.FindString("index.html")
 
 	r.Methods("GET").Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		t := template.Must(template.New("index.html").Funcs(template.FuncMap{
@@ -22,7 +18,7 @@ func getRoutes(s *Server, r *mux.Router) *mux.Router {
 				a, _ := json.Marshal(v)
 				return string(a)
 			},
-		}).ParseFiles(index))
+		}).ParseFiles("static/index.html"))
 		t.Execute(w, GetUser(r))
 		// fmt.Printf("%+v\n", t.Execute(w, GetUser()))
 	})
